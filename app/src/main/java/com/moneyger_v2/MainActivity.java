f34e9c5949f8ae93fa3d0fffaf7c97570699e95d
Richelle Anne Natividad <richellenatividad@gmail.com>
2021-04-02 02:52:14 -0700

    moneyger_v2 updates

--- app/src/main/java/com/moneyger_v2/MainActivity.java
+++ app/src/main/java/com/moneyger_v2/MainActivity.java
@@ -2,7 +2,9 @@ package com.moneyger_v2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -13,11 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //firebase reference
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("Budget").child("Food");
         myRef.setValue("1500");
-
         DatabaseReference myRef2 = database.getReference("Expenses").child("iPhone");
         myRef2.setValue("150");
+    }
+
+    public void loadBudget (View view) {
+        Intent intent = new Intent(this, BudgetActivity.class);
+        startActivity(intent);
+    }
+
+    public void loadExpense (View view) {
+        Intent intent = new Intent(this, ExpenseActivity.class);
+        startActivity(intent);
+    }
+
+    public void loadStatus (View view) {
+        Intent intent = new Intent(this, StatusActivity.class);
+        startActivity(intent);
+    }
+
+    public void loadReminders (View view) {
+        Intent intent = new Intent(this, RemindersActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/moneyger_v2/RemindersActivity.java
@@ -0,0 +1,14 @@
+package com.moneyger_v2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RemindersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminders);
+    }
+}

22b90ebfbfa4c5c91a0fa7a77f7a5b9864363e61
slacama <sariahacama@gmail.com>
2021-04-03 11:22:57 +0800

    made budget, expense, status, reminders activities to fragments added a content activity for all fragments added buttons to expense, reminders, status

--- app/src/main/java/com/moneyger_v2/MainActivity.java
+++ app/src/main/java/com/moneyger_v2/MainActivity.java
@@ -15,64 +15,16 @@ import com.google.firebase.database.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
 
-    BottomNavigationView bottomNavigationView;
-    View view;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setTitle("Moneyger -  Money Manager App");
-
-        bottomNavigationView = findViewById(R.id.menuBar);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
-                switch (item.getItemId()) {
-
-                    case R.id.budgetButton:
-                        loadBudget(view);
-                        return true;
-
-                    case R.id.expenseButton:
-                        loadExpense(view);
-                        return true;
-
-                    case R.id.statusButton:
-                        loadStatus(view);
-                        return true;
-
-                    case R.id.reminderButton:
-                        loadReminders(view);
-                        return true;
-
-                    default:
-                        return false;
-
-                }
-            }
-        });
+        setTitle("Moneyger - Money Manager App");
     }
 
-    public void loadBudget (View view) {
-        Intent intent = new Intent(this, BudgetActivity.class);
-        startActivity(intent);
-    }
-
-    public void loadExpense (View view) {
-        Intent intent = new Intent(this, ExpenseActivity.class);
-        startActivity(intent);
-    }
-
-    public void loadStatus (View view) {
-        Intent intent = new Intent(this, StatusActivity.class);
-        startActivity(intent);
-    }
-
-    public void loadReminders (View view) {
-        Intent intent = new Intent(this, RemindersActivity.class);
+    public void loadContent (View view) {
+        Intent intent = new Intent(this, ContentActivity.class);
         startActivity(intent);
     }
 }

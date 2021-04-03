0fc2d777f2c07009cbb008d0698cb12eef68fc4a
slacama <sariahacama@gmail.com>
2021-04-03 13:46:23 +0800

    added fragments

--- app/src/main/java/com/moneyger_v2/ExpenseFragment.java
+++ app/src/main/java/com/moneyger_v2/ExpenseFragment.java
@@ -3,17 +3,55 @@ package com.moneyger_v2;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class ExpenseFragment extends Fragment {
+    View view;
+    Button new_Expense, edit_Expense, delete_Expense;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_expense, container, false);
+        view = inflater.inflate(R.layout.fragment_expense, container, false);
+        new_Expense = (Button) view.findViewById(R.id.addExpense);
+        edit_Expense = (Button) view.findViewById(R.id.editExpense);
+        delete_Expense = (Button) view.findViewById(R.id.deleteExpense);
+
+        new_Expense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setFragment(new newExpense());
+            }
+        });
+
+        edit_Expense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setFragment(new editExpense());
+            }
+        });
+
+        delete_Expense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setFragment(new deleteExpense());
+            }
+        });
+
+        return view;
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.expenseFrame, fragment);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/moneyger_v2/RemindersFragment.java
+++ app/src/main/java/com/moneyger_v2/RemindersFragment.java
@@ -3,17 +3,55 @@ package com.moneyger_v2;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class RemindersFragment extends Fragment {
+    View view;
+    Button new_Reminder, edit_Reminder, delete_Reminder;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reminders, container, false);
+        view = inflater.inflate(R.layout.fragment_reminders, container, false);
+        new_Reminder = (Button) view.findViewById(R.id.newReminder);
+        edit_Reminder = (Button) view.findViewById(R.id.editReminder);
+        delete_Reminder = (Button) view.findViewById(R.id.deleteReminder);
+
+        new_Reminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setFragment(new newReminder());
+            }
+        });
+
+        edit_Reminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setFragment(new editReminder());
+            }
+        });
+
+        delete_Reminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setFragment(new deleteReminder());
+            }
+        });
+
+        return view;
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.reminderFrame, fragment);
+        fragmentTransaction.commit();
     }
 }

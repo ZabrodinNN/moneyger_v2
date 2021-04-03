a21b131abec6fe9859e57e975e850c4a1e9f79b8
Kimberlyka24 <kimberlyka24@gmail.com>
2021-04-03 19:05:07 +0800

    1

--- app/src/main/java/com/moneyger_v2/StatusFragment.java
+++ app/src/main/java/com/moneyger_v2/StatusFragment.java
@@ -1,19 +1,63 @@
 package com.moneyger_v2;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class StatusFragment extends Fragment {
+    View view;
+    TextView text;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    Button showBudget;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_status, container, false);
+        view = inflater.inflate(R.layout.fragment_status, container, false);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Budget");
+        text = view.findViewById(R.id.textView);
+        showBudget = (Button) view.findViewById(R.id.selectBudget);
+        showBudget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReference.addValueEventListener(new ValueEventListener() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        Map<String, Object> td = (HashMap<String, Object>) snapshot.getValue();
+                        text.setText(td.toString());
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(getActivity(), "Failed to get data.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+
+        return view;
     }
 }

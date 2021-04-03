1394049259ac7bc00696cf3c859da68ff465b56f
slacama <sariahacama@gmail.com>
2021-04-03 10:12:15 +0800

    menu bar functional

--- app/src/main/java/com/moneyger_v2/MainActivity.java
+++ app/src/main/java/com/moneyger_v2/MainActivity.java
@@ -1,15 +1,22 @@
 package com.moneyger_v2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setTitle("Moneyger -  Money Manager App");
+
+        bottomNavigationView = findViewById(R.id.menuBar);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+                switch (item.getItemId()) {
+
+                    case R.id.budgetButton:
+                        loadBudget(view);
+                        return true;
+
+                    case R.id.expenseButton:
+                        loadExpense(view);
+                        return true;
+
+                    case R.id.statusButton:
+                        loadStatus(view);
+                        return true;
+
+                    case R.id.reminderButton:
+                        loadReminders(view);
+                        return true;
+
+                    default:
+                        return false;
+
+                }
+            }
+        });
     }
 
     public void loadBudget (View view) {

32b2779e3753730c082b71a8a910215372837265
Kimberlyka24 <kimberlyka24@gmail.com>
2021-04-03 19:50:54 +0800

    Added "add expenses" function.

--- app/src/main/java/com/moneyger_v2/newExpense.java
+++ app/src/main/java/com/moneyger_v2/newExpense.java
@@ -4,16 +4,60 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class newExpense extends Fragment {
+    View view;
+    Button fragmentButton;
+    String name;
+    int cost;
+    EditText expenseName;
+    EditText expenseValue;
+    Button saveExpense;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_expense, container, false);
+        view = inflater.inflate(R.layout.fragment_new_expense, container, false);
+
+        expenseName = view.findViewById(R.id.expenseName);
+        expenseValue = view.findViewById(R.id.expenseValue);
+
+        saveExpense = (Button) view.findViewById(R.id.saveExpense);
+        saveExpense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(expenseName.getText().toString()) || expenseName.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(getActivity(), "Please input the expense name", Toast.LENGTH_SHORT).show();
+                } else {
+                    name = expenseName.getText().toString();
+                    if (TextUtils.isEmpty(expenseValue.getText().toString())) {
+                        Toast.makeText(getActivity(), "Please input the expense cost", Toast.LENGTH_SHORT).show();
+                    } else {
+                        cost = Integer.parseInt( expenseValue.getText().toString());
+                        savedBudget expense = new savedBudget(name, Integer.toString(cost));
+                        showToast();
+                        FirebaseDatabase database = FirebaseDatabase.getInstance();
+                        DatabaseReference myRef = database.getReference("Expenses").child(name);
+                        myRef.setValue(expense);
+                    }
+                }
+            }
+        });
+
+        return view;
+    }
+    public void showToast() {
+        Toast.makeText(getActivity(), "Your expense has been saved", Toast.LENGTH_SHORT).show();
     }
 }

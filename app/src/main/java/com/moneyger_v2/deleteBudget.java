6f8a41cedd665190b8b90d75e857a6f163430492
Kimberlyka24 <kimberlyka24@gmail.com>
2021-04-03 21:17:11 +0800

    Added delete budget.

--- app/src/main/java/com/moneyger_v2/deleteBudget.java
+++ app/src/main/java/com/moneyger_v2/deleteBudget.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,7 +22,10 @@ import com.google.firebase.database.ValueEventListener;
 
 public class deleteBudget extends Fragment {
     View view;
+    EditText nameBudget;
     Button fragmentButton;
+    Button deleteButton;
+    String name;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,6 +33,22 @@ public class deleteBudget extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_delete_budget, container, false);
         fragmentButton = (Button)view.findViewById(R.id.deleteBudget);
+
+        nameBudget = view.findViewById(R.id.nameBudget);
+        deleteButton = (Button) view.findViewById(R.id.deleteBudget);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(nameBudget.getText().toString()) || nameBudget.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(getActivity(), "Please input the budget name", Toast.LENGTH_SHORT).show();
+                } else {
+                    name = nameBudget.getText().toString();
+                    DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Budget").child(name);
+                    reference.removeValue();
+                }
+            }
+        });
+
         return view;
     }
 }
